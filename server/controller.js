28411cc9aaf3a15ff60248751f3fa01099dc8907
Dominik Gersak <domgersak@gmail.com>
2019-04-06 18:08:51 +0200

    server updates made on raspberry

--- server/controller.js
+++ server/controller.js
@@ -1,35 +1,63 @@
-var Gpio = require('pigpio').Gpio, //include pigpio to interact with the GPIO
-    ledRed = new Gpio(4, { mode: Gpio.OUTPUT }), //use GPIO pin 4 as output for RED
-    ledGreen = new Gpio(17, { mode: Gpio.OUTPUT }), //use GPIO pin 17 as output for GREEN
-    ledBlue = new Gpio(27, { mode: Gpio.OUTPUT }), //use GPIO pin 27 as output for BLUE
-    redRGB = 255, //set starting value of RED variable to off (255 for common anode)
-    greenRGB = 255, //set starting value of GREEN variable to off (255 for common anode)
-    blueRGB = 255; //set starting value of BLUE variable to off (255 for common anode)
+const Gpio = require('pigpio').Gpio; //include pigpio to interact with the GPIO
 
-// RESET RGB LED
-ledRed.digitalWrite(1); // Turn RED LED off
-ledGreen.digitalWrite(1); // Turn GREEN LED off
-ledBlue.digitalWrite(1); // Turn BLUE LED off
+const ledRed = new Gpio(4, { mode: Gpio.OUTPUT }); //use GPIO pin 4 as output for RED
+const ledGreen = new Gpio(17, { mode: Gpio.OUTPUT }); //use GPIO pin 17 as output for GREEN
+const ledBlue = new Gpio(27, { mode: Gpio.OUTPUT }); //use GPIO pin 27 as output for BLUE
 
 function getRandomArbitrary (min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function random (req, res) {
-    //for common anode RGB LED  255 is fully off, and 0 is fully on, so we have to change the value from the client
-    redRGB = 255 - parseInt(getRandomArbitrary(0, 255));
-    greenRGB = 255 - parseInt(getRandomArbitrary(0, 255));
-    blueRGB = 255 - parseInt(getRandomArbitrary(0, 255));
+function reset () {
+    // RESET RGB LED
+    ledRed.digitalWrite(0); // Turn RED LED off
+    ledGreen.digitalWrite(0); // Turn GREEN LED off
+    ledBlue.digitalWrite(0); // Turn BLUE LED off
+}
 
-    ledRed.pwmWrite(redRGB); //set RED LED to specified value
-    ledGreen.pwmWrite(greenRGB); //set GREEN LED to specified value
-    ledBlue.pwmWrite(blueRGB); //set BLUE LED to specified value
+function setRGB (req, res) {
+    
+    const r = req.body.r;
+    const g = req.body.g;
+    const b = req.body.b;
+    
+    ledRed.pwmWrite(r);
+    ledGreen.pwmWrite(g);
+    ledBlue.pwmWrite(b);
+    
+    const log = `Setting colors to RGB (${r}, ${g}, ${b});`;
+    console.log(log);
+    res.send(log);
+}
 
-    const log = "rbg: " + redRGB + ", " + greenRGB + ", " + blueRGB;
-    console.log(log); //output converted to console
+function randomRGB (req, res) {
+
+    const r = parseInt(getRandomArbitrary(0, 255));
+    const g = parseInt(getRandomArbitrary(0, 255));
+    const b = parseInt(getRandomArbitrary(0, 255));
+
+    ledRed.pwmWrite(r);
+    ledGreen.pwmWrite(g);
+    ledBlue.pwmWrite(b);
+
+    const log = "rbg: " + r + ", " + g + ", " + b;
+    console.log(log);
     res.send(log)
 }
 
+function turnOff (req, res) {
+    
+    reset();
+        
+    const log = "turned off";
+    console.log(log);
+    res.send(log);
+}
+
+reset();
+
 module.exports = {
-    random: random
+    randomRGB: randomRGB,
+    setRGB: setRGB,
+    turnOff: turnOff
 }
