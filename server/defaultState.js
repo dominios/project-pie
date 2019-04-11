33c039e1637eec43f97615f63cf50c5ed11312b3
Dominik Gersak <dgersak@sk.ibm.com>
2019-04-11 14:03:56 +0200

    added more default colors

--- server/defaultState.js
+++ server/defaultState.js
@@ -36,26 +36,138 @@ module.exports = {
         },
         {
             id: 3,
-            name: 'Green',
+            name: 'Crimson',
+            configuration: {
+                branch1: [220, 20, 60],
+                branch2: [220, 20, 60]
+            }
+        },
+        {
+            id: 4,
+            name: 'Pink',
+            configuration: {
+                branch1: [255, 20, 147],
+                branch2: [255, 20, 147],
+            }
+        },
+        {
+            id: 5,
+            name: 'Fuchsia',
+            configuration: {
+                branch1: [255, 0, 255],
+                branch2: [255, 0, 255]
+            }
+        },
+        {
+            id: 6,
+            name: 'Viloet',
+            configuration: {
+                branch1: [153, 50, 204],
+                branch2: [153, 50, 204],
+            }
+        },     
+        {
+            id: 7,
+            name: 'Orange',
+            configuration: {
+                branch1: [255, 165, 0],
+                branch2: [255, 165, 0],
+            }
+        },
+        {
+            id: 8,
+            name: 'Gold',
+            configuration: {
+                branch1: [255, 215, 0],
+                branch2: [255, 215, 0],
+            }
+        },
+        {
+            id: 9,
+            name: 'Yellow',
+            configuration: {
+                branch1: [255, 255, 0],
+                branch2: [255, 255, 0]
+            }
+        },
+        {
+            id: 10,
+            name: 'Green Apple',
+            configuration: {
+                branch1: [173, 255, 47],
+                branch2: [173, 255, 47],
+            }
+        },
+        {
+            id: 11,
+            name: 'Lime',
             configuration: {
                 branch1: [0, 255, 0],
                 branch2: [0, 255, 0]
             }
         },
         {
-            id: 4,
-            name: 'Blue',
+            id: 12,
+            name: 'Green',
             configuration: {
-                branch1: [0, 0, 255],
-                branch2: [0, 0, 255]
+                branch1: [0, 128, 0],
+                branch2: [0, 128, 0]
             }
         },
         {
-            id: 5,
+            id: 13,
+            name: 'Teal',
+            configuration: {
+                branch1: [0, 128, 128],
+                branch2: [0, 128, 128]
+            }
+        },
+        {
+            id: 14,
             name: 'Cyan',
             configuration: {
                 branch1: [0, 255, 255],
                 branch2: [0, 255, 255]
+            }
+        },
+        {
+            id: 15,
+            name: 'Turquoise',
+            configuration: {
+                branch1: [54, 224, 208],
+                branch2: [54, 224, 208]
+            }
+        },
+        {
+            id: 16,
+            name: 'Steel Blue',
+            configuration: {
+                branch1: [70, 130, 180],
+                branch2: [70, 130, 180]
+            }
+        },
+        {
+            id: 17,
+            name: 'Deep Sky Blue',
+            configuration: {
+                branch1: [0, 191, 255],
+                branch2: [0, 191, 255]
+            }
+        },
+        {
+            id: 18,
+            name: 'Royal Blue',
+            configuration: {
+                branch1: [65, 105, 225],
+                branch2: [65, 105, 225]
+            }
+        },
+        {
+            id: 19,
+            name: 'Blue',
+            configuration: {
+                branch1: [0, 0, 255],
+                branch2: [0, 0, 255]
             }
         }
     ]
